da68e16ca82a4bd8f5d71c22f43c89a024622466
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 17:44:39 -0800

    Update Ten_Guesses.py

--- Ten_Guesses.py
+++ Ten_Guesses.py
@@ -8,34 +8,24 @@ print()
 num_guess = 10
 
 while num_guess > 0:
-
     guess = input("Player 2: What is your guess? ")
 
     if guess == answer:
-
         print("Congratulations! You got it!")
-
         break
 
     else:
-
         num_guess -= 1
-
         print("Incorrect guess. Ask the other player a yes/no question and try again.")
 
         if num_guess > 0:
-
             print("You have " + str(num_guess) + " guesses remaining. ")
-
             print()
-
             guess = input("Player 2, ask Player 1 a yes-or-no question about their object. (Hit enter once they've answered.)")
 
         print()
 
 if num_guess == 0:
-
     print("Sorry, you lost!")
     print("Start another game to play again!")
-
     print("The correct answer was " + str(answer) + " !")
